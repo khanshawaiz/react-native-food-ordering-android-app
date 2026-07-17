7d7b81a91ffa82f1407c4a4bbe3aef72088f25c4
ishawaizkhan <ashawaizkhan365@gmail.com>
2026-07-17 11:30:05 +0500

    Adding tabs

---
+++ app/(tabs)/two.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
---
+++ commands.md
@@ -0,0 +1,16 @@
+Before coding:
+1. Plug phone via USB
+2. Enable USB Debugging
+
+Every time you start:
+nvm use 18.20.8
+cd "C:\Users\Shawaiz\Desktop\React Native\FoodOrdering"
+C:\Users\Shawaiz\Downloads\platform-tools\adb.exe reverse tcp:8081 tcp:8081
+npx expo start
+
+
+
+After coding:
+git add .
+git commit -m "Your message"
+git push
