6cf3ca838ff01e53a1557c35e2f82b5197f42379
ishawaizkhan <ashawaizkhan365@gmail.com>
2026-07-21 23:16:39 +0500

    Add size selector and Add to cart button

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,11 +1,27 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable} from "react-native";
 import { useLocalSearchParams, Stack } from "expo-router";
 import products from "@data/products";
 import { defaultPizzaImage } from "@/components/ProductListItem";
+import { useState } from "react";
+import Button from "@/components/Button";
+
+
+
+const sizes = ['S','M','L','XL'];
 
 export default function ProductDetails() {
   const { id } = useLocalSearchParams();
+
+  
+ const [selectedSize, setSelectedSize] = useState('S');
+
+
   const product = products.find((p) => p.id.toString() === id);
+
+ const addToCart = () => {
+     console.warn('Adding to cart');
+ }
+
 
   if (!product) {
     return <Text>Product not found</Text>;
@@ -19,27 +35,70 @@ export default function ProductDetails() {
         style={styles.image}
         resizeMode="cover"
       />
+   
+       <Text>Select size</Text>
+       <View style = {styles.sizes}>
+          {sizes.map((size)  => (
+           <Pressable onPress={() =>  {
+            setSelectedSize(size);
+          }}
+            style = {[styles.size, { backgroundColor : selectedSize === size ? 'gainsboro' : 'white'}]} 
+            key = {size}
+            >
+        
+         <Text style = {[styles.sizeText, {color : selectedSize === size ?  'black' : 'gray'}]}>
+          {size}</Text>
+        
+
+         </Pressable>
+       ))}
+        </View>
+
+
       <Text style={styles.price}>${product.price}</Text>
+      <Button onPress={addToCart}  text = "Add to cart" />
+      
+
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
-    flex: 1,
-    padding: 20,
-    alignItems: 'center',
-  },
-  image: {
-    width: '80%',
-    aspectRatio: 1,
-    borderRadius: 9999,
-    marginVertical: 20,
-  },
-  price: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'black',
-  },
+  backgroundColor: 'white',
+  flex: 1,
+  padding: 20,
+  alignItems: 'center',
+},
+image: {
+  width: '80%',
+  aspectRatio: 1,
+  borderRadius: 9999,
+  marginVertical: 20,
+},
+sizes: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  width: '80%',
+  marginVertical: 20,
+},
+size: {
+  backgroundColor: 'gainsboro',
+  width: 55,
+  aspectRatio: 1,
+  borderRadius: 9999,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+sizeText: {
+  fontSize: 20,
+  fontWeight: '500',
+},
+price: {
+  fontSize: 18,
+  fontWeight: 'bold',
+  color: 'black',
+},
 });
